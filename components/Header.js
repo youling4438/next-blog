1a33f5eacf7976f912bf7f00d58a5ce2eaced1a2
youling4438 <youling0106@163.com>
2020-09-19 00:57:37 +0800

    refactor header components

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,56 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Router from 'next/router'
 import { Row, Col, Menu } from 'antd'
-import { SmileOutlined, HomeOutlined, YoutubeOutlined, } from '@ant-design/icons'
+import { HomeOutlined, YoutubeOutlined, MessageOutlined, SmileOutlined } from '@ant-design/icons'
 import styles from '../styles/components/header.module.css'
-const Header = () => (
-	<div className={styles.header}>
-		<Row type="flex" justify="center">
-			<Col xs={24} sm={24} md={10} lg={15} xl={12} >
-				<span className={styles['header-logo']}>幽灵自醒</span>
-				<span className={styles['header-text']}>前端开发---专注PC重构</span>
-			</Col>
-			<Col xs={0} sm={0} md={14} lg={8} xl={6} >
-				<Menu mode="horizontal">
-					<Menu.Item key="home">
-						<HomeOutlined />
-						首页
-					</Menu.Item>
-					<Menu.Item key="video">
-						<YoutubeOutlined />
-						视频
-					</Menu.Item>
-					<Menu.Item key="life">
-						<SmileOutlined />
-						生活
-					</Menu.Item>
-				</Menu>
-			</Col>
-		</Row>
-	</div>
-)
+import servicePath from '../config/apiUrl'
+const Header = () => {
+	const [navList, setNavList] = useState([])
+	useEffect(() => {
+		const getNavData = async () => {
+			await axios(servicePath.getTypeInfo).then((res) => {
+				setNavList(res.data.data)
+			})
+		};
+		getNavData()
+	}, [])
+	const handleClick = (e)=>{
+        if(e.key==0){
+            Router.push('/')
+        }else{
+            Router.push('/list?id='+e.key)
+        }
+    }
+
+	return (
+		<div className={styles.header}>
+			<Row type="flex" justify="center">
+				<Col xs={24} sm={24} md={10} lg={15} xl={12} >
+					<span className={styles['header-logo']}>幽灵自醒</span>
+					<span className={styles['header-text']}>前端开发---专注PC重构</span>
+				</Col>
+				<Col xs={0} sm={0} md={14} lg={8} xl={6} >
+					<Menu mode="horizontal"  onClick={handleClick} >
+						<Menu.Item key='0'>
+							<HomeOutlined />
+							首页
+						</Menu.Item>
+						{ navList.map(item=>{
+							return (
+								<Menu.Item key={item.id}>
+									{ item.icon === 'youtube' && <YoutubeOutlined /> }
+									{ item.icon === 'message' && <MessageOutlined /> }
+									{ item.icon === 'smile' && <SmileOutlined /> }
+									{item.typeName}
+								</Menu.Item>
+							)
+						}) }
+					</Menu>
+				</Col>
+			</Row>
+		</div>
+	)
+}
 
 export default Header;
--- config/apiUrl.js
+++ config/apiUrl.js
@@ -2,6 +2,7 @@ let ipUrl = 'http://127.0.0.1:7001/'
 let servicePath = {
     getArticleList:ipUrl + 'getArticleList' ,  //  首页文章列表接口
     getArticleDetailById:ipUrl + 'getArticleDetailById/',  // 文章详细页内容接口 ,需要接收参数
+    getTypeInfo:ipUrl + 'getTypeInfo',  // 文章详细页内容接口 ,需要接收参数
 
 }
 export default servicePath;
